239c2a4fef8041b85c0cb03e832ac01ee9650e32
Dzung DH <dzung.dh3510@gmail.com>
2022-07-11 11:40:25 +0700

    add wallet address input at index.html

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -3,18 +3,35 @@ const METAMASK_CONNECT_FAILED = 'Failed to connect to MetaMask. Please try again
 const MSW_URL = "/multisig-wallet.html";
 const Metamask = window.ethereum;
 
+document.querySelector('.input-wrapper > input').addEventListener('focus', event => {
+   let label = document.querySelector('.input-wrapper > label');
+   label.style.top = '-20px';
+   label.style.fontSize = '0.8rem';
+});
+
+document.querySelector('.input-wrapper > input').addEventListener('focusout', event => {
+   if (event.target.value === '') {
+      let label = document.querySelector('.input-wrapper > label');
+      label.style.top = '0px';
+      label.style.fontSize = 'inherit';
+   }
+});
+
+document.querySelector('.input-wrapper > input').addEventListener('input', event => {
+   document.querySelector('#connect-metamask').disabled = event.target.value === '';
+});
+
 if (Metamask == undefined || !Metamask.isMetaMask) {
    alert(METAMASK_UNINSTALLED_MSG);
 } else {
    window.ethereum._metamask.isUnlocked()
    .then(isUnlocked => {
       if (isUnlocked) {
-         window.location.replace(MSW_URL);
+         // window.location.replace(MSW_URL);
       }
       else {
          document.querySelector('#connect-metamask').addEventListener('click', event => {
             event.target.disabled = true;
-            document.querySelector('#connect-metamask').style.opacity = 0.5;
          
             Metamask.request({ method: 'eth_requestAccounts' })
                .then(data => {
@@ -25,7 +42,6 @@ if (Metamask == undefined || !Metamask.isMetaMask) {
                   console.log(error);
                   alert(METAMASK_CONNECT_FAILED);
                   event.target.disabled = false;
-                  document.querySelector('#connect-metamask').style.opacity = 'unset';
                })
          });
       }
